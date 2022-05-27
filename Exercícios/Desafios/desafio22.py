21b924fc029c9a4bba11fa5aa6128d3b91c1e60c
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-05-26 23:23:56 -0300

    Separador de casas posições decimais

---
+++ Exercícios/Desafios/desafio22.py
@@ -0,0 +1,15 @@
+from ntpath import join
+
+
+nm = input('Digite seu nome completo: ')
+
+caps = nm.upper()
+bx = nm.lower()
+
+palavras = nm.split()
+semEspaço = ''.join(palavras)
+letrasSemEspaço = len(semEspaço)
+priNm = palavras[0]
+priLtrs = len(priNm)
+
+print('Seu nome com CAPS LOCK: {} \n Seu nome falando baixinho: {} \n Quantas letras sem contar espaços: {} \n Quantidade de letras no primeiro nome: {}'.format(caps, bx, letrasSemEspaço, priLtrs))
