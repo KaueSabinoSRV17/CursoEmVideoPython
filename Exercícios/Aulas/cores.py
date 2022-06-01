00167d13f42e1528654be147a81438ad0a224101
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-01 00:38:37 -0300

    Aula 11 Bonûs - Cores com padrão ANSI

---
+++ Exercícios/Aulas/cores.py
@@ -0,0 +1,9 @@
+# Vamos agora trabalhar com cores no seu terminal
+
+# Devemos usar inicialmente o código ANSI - Escape Sequence
+
+# Para trabalhar com cores, devemos usar esta sintaxe: \033[style, texto, backgroundm
+
+# style, texto e back acima são substituidos por números, onde o 0 quer dizer o padrão
+
+print('\033[4;30;45mOlá Mundo!')
