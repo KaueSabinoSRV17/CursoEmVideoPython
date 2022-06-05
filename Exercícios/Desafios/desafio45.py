9708a93fb3c022b81c7b6c783e380b0344c332cc
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-05 20:35:58 -0300

    Desafio 45: Pedra, Papel, Tesoura

--- Exercícios/Desafios/desafio45.py
+++ Exercícios/Desafios/desafio45.py
@@ -1,21 +1,92 @@
 # Jokenpo | Pedra Papel Tesoura
 
 import random
+import time
 
 op = ['Pedra', 'Papel', 'Tesoura']
 
+rs = ['Temos um empate!', 'O Jogador ganhou!', 'O CPU ganhou!', 'O jogador escolheu uma opção inválida!', 'O CPU escolheu uma opção inválida!']
+
 CPU = random.randint(0, 2)
 
-Player = int(input('Digite 0 para {}\n1 para {}\n2 para {}\n'.format(op[0], op[1], op[3])))
+Player = int(input('Pense em {} (0), {} (1) ou {} (2): '.format(op[0], op[1], op[2])))
 
-if CPU == Player:
+linha = '-=-' * 20
 
-    print('{} x {}\nEmpate!'.format(op[Player], op[CPU]))
+time.sleep(.5)
 
-elif CPU == 0 and Player == 1:
+print('JO')
 
-    print('{} X {}\nVitória do CPU!'.format(op[Player], op[CPU]))
+time.sleep(.5)
 
-elif CPU == 1 and Player == 2:
+print('KEN')
 
-    print('{} X {}\nVitória do Player!'.format(op[Player], op[CPU]))
+time.sleep(.5)
+
+print('PO!')
+
+if CPU == 0:
+
+    if Player == 0:
+
+        i = 0
+
+    elif Player == 1:
+
+        i = 1
+
+    elif Player == 2:
+
+        i = 2
+
+    else:
+
+        i = 3
+
+elif CPU == 1:
+
+    if Player == 0:
+
+        i = 2
+
+    elif Player == 1:
+
+        i = 0
+
+    elif Player == 2:
+
+        i = 2
+
+    else:
+
+        i = 3
+
+elif CPU == 2:
+
+    if Player == 0:
+
+        i = 1
+
+    elif Player == 1:
+
+        i = 2
+
+    elif Player == 2:
+
+        i = 0
+
+    else:
+
+        i = 3
+
+else:
+
+    print(rs[4])
+
+print(linha)
+
+print(rs[i])
+
+print('O Jogador jogou {}\nO CPU jogou {}'.format(op[Player], op[CPU]))
+
+print(linha)
