515153116c377ab950074ab6d583eccb7d7111c9
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-05 13:14:12 -0300

    Desafio 39: Alistamento Militar

---
+++ Exercícios/Desafios/desafio39.py
@@ -0,0 +1,15 @@
+# Alistamento Militar
+
+id = int(input('Quantos anos completos o senhor tem? \n'))
+
+if id < 18:
+
+    print('\n Você ainda irá se alistar')
+
+elif id == 18:
+
+    print('Aliste-se já!')
+
+else:
+
+    print('Você está dispensado!')
