da16b0de68082b2174861ab34af795aebfc34101
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-05 20:55:03 -0300

    Repetição For

---
+++ Exercícios/Aulas/repetições.py
@@ -0,0 +1,16 @@
+"""
+    For
+
+    Devemos usar repetições para todo código que vai se repetir por muito tempo. em todas as situações, devemos evitar a repetição de código
+
+    Segundo a necessidade, podemos combinar repetições e condições, de todas as formas que acharmos melhor.
+
+"""
+
+for c in range(0, 11):
+
+    print(c)
+
+print('FIM')
+
+# Neste código, por exemplo, podemos repetir a exibição de diversos números
