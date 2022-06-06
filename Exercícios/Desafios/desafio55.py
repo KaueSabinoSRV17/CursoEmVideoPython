f9fd6d5a120b581d5b6cfbc31856e96dc72b0447
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-06 00:05:00 -0300

    Resolução Desafio 55

--- Exercícios/Desafios/desafio55.py
+++ Exercícios/Desafios/desafio55.py
@@ -3,17 +3,25 @@
 ma = 0
 
 mn = 0
-
+    
 for i in range(0, 5):
 
     ps = float(input('Digite um peso em Kg (faltam {}): '.format(5 - i)))
 
-    if ps > ma:
+    if i == 0:
 
         ma = ps
+        
+        mn = ps
 
     else:
 
-        mn = ps
+        if ps > ma:
+
+            ma = ps
+
+        else:
+
+            mn = ps
 
 print('O maior peso foi {:.2f}Kg e o menor foi {:.2f}Kg'.format(ma, mn))
