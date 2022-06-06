a78a9a6aacc8dfe7256e88e82a6e503e4ade7f24
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-05 21:56:13 -0300

    Errata: Desafio 47 é Números pares entre 0 e 50

--- Exercícios/Desafios/desafio47.py
+++ Exercícios/Desafios/desafio47.py
@@ -1,9 +1,7 @@
-s = 0
+for i in range(0, 51):
 
-for i in range(0, 501):
+    if i % 2 == 0:
 
-    if i % 3 == 0 and i % 2 == 1:
+        print(i)
 
-        s += i
-
-print('A soma de todos os número ímpares divisíveis por 3 entre 0 e 500 é {}'.format(s))
+print('FIm')
