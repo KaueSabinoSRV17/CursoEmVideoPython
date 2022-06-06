00353faa2d83ed5a0a2e0ba424419c8b501d6408
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-05 23:38:22 -0300

    Desafio 52: Números Primos

--- Exercícios/Desafios/desafio52.py
+++ Exercícios/Desafios/desafio52.py
@@ -1,2 +1,19 @@
 # Números primos
 
+c = 0
+
+n = int(input('Digite um número: '))
+
+for i in range(n, 0, -1):
+
+    if n % i == 0:
+
+        c += 1
+
+if c > 2:
+
+    print('O número não é Primo!')
+
+else:
+
+    print('O número é Primo!')
