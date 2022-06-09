f864f7077dbef64ca3fd6b25d7cea9753df6f24f
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-09 20:56:27 -0300

    Adição de novos números, maior e menor

--- Exercícios/Desafios/desafio59.py
+++ Exercícios/Desafios/desafio59.py
@@ -2,6 +2,7 @@ import os
 
 n1 = float(input('Digite o primeiro valor: '))
 n2 = float(input('Digite o segundo valor: '))
+linha = '-==-' * 20
 
 menu = '''
 
@@ -9,12 +10,22 @@ menu = '''
     [2] - Subtração
     [3] - Multiplicação
     [4] - Divisão
+    [5] - Maior
+    [6] - Novos Números
     [0] - Sair do Programa
 
 '''
 
 
-op = int(input(menu))
+op = int(input('''
+
+{}
+
+{}
+
+{}
+
+'''.format(linha, menu, linha)))
 
 while op != 0:
 
@@ -23,18 +34,44 @@ while op != 0:
         print('\n {}'.format(n1 + n2))
         op = int(input(menu))
 
-    if op == 2:
+    elif op == 2:
 
         print('\n {}'.format(n1 - n2))
         op = int(input(menu))        
 
-    if op == 3:
+    elif op == 3:
 
         print('\n {}'.format(n1 * n2))
         op = int(input(menu))        
 
-    if op == 4:
+    elif op == 4:
 
         print('\n {}'.format(n1 / n2))
-        op = int(input(menu))        
-        
+        op = int(input(menu))
+
+    elif op == 5:
+
+        if n1 > n2:
+
+            mr = n1
+
+        if n1 < n2:
+
+            mr = n2
+
+        print('O maior foi {}'.format(mr))        
+
+    elif op == 6:
+
+        print('Informe novos números novamente: ')
+        n1 = float(input('Digite o primeiro número: '))
+        n2 = float(input('Digite o segundo número: '))
+
+        op = int(input(menu))
+    
+    else:
+
+        print('Opção Inválida! Digite entre uma das opções descritas no menu')
+        op = int(input(menu))
+
+print('Fim')
