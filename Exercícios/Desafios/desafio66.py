0091b384cdabe92218e35ece68e3c274ca862314
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-10 01:29:00 -0300

    Resolução Desafio 66: Contagem e Soma

--- Exercícios/Desafios/desafio66.py
+++ Exercícios/Desafios/desafio66.py
@@ -1,7 +1,13 @@
+c = s = 0
+
 while True:
+    
     n = float(input('Digite um valor: '))
     
     if n == 999:
         break
 
-print('Fim')
+    s += n
+    c += 1
+
+print(f'Você digitou {c} números, resultando em uma soma {s}')
