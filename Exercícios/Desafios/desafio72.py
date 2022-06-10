5099de6667994c801b3d16ff8946f08a00bfb83b
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-10 14:27:18 -0300

    Resolução Desafio 72: Fim determinado pelo usuário

--- Exercícios/Desafios/desafio72.py
+++ Exercícios/Desafios/desafio72.py
@@ -1,9 +1,22 @@
 ct = 'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte'
 
-op = int(input('Digite um valor entre 0 e 20 para ser mostrado por extenso: '))
+while True:
 
-while op > 20 or op < 0:
+    op = int(input('Digite um valor entre 0 e 20 para ser mostrado por extenso: '))
 
-    op = int(input(f'DIGITE APENAS UM VALOR ENTRE 0 E 20! '))
+    while op > 20 or op < 0:
 
-print(f'{op} por extenso é igual a {ct[op]}')
+        op = int(input(f'DIGITE APENAS UM VALOR ENTRE 0 E 20! '))
+
+    print(f'{op} por extenso é igual a {ct[op]}')
+
+    dlg = input('Deseja continuar? [S/N]: ')
+
+    while dlg not in 'nNsS':
+
+        dlg = input('DIGITE APENAS SIM OU NÃO: ')
+
+    if dlg in 'nN':
+        break
+
+print('FIm. Volte sempre!')
