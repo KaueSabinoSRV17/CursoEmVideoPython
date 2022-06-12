38c34a350a64e5d7c740d43c26fea00ca6fffc4b
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-12 13:49:04 -0300

    Resolução Desafio 78: Posição do menor e maior

--- Exercícios/Desafios/desafio78.py
+++ Exercícios/Desafios/desafio78.py
@@ -4,4 +4,17 @@ for i in range(0, 4):
 
     vls.append(int(input('Digite um valor para ser armazenado na lista: ')))
 
-print(f'O maior valor foi {max(vls)}\nO menor valor foi {min(vls)}')
+ma = max(vls)
+mn = min(vls)
+
+print(f'O maior valor foi {ma}\nO menor valor foi {mn}')
+
+for i, v in enumerate(vls):
+
+    if v == ma:
+
+        print(f'O maior valor estava na {i}ª posição')
+
+    if v == mn:
+
+        print(f'O menor valor estava na {i}ª posição')
