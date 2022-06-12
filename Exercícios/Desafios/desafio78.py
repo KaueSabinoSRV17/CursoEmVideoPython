499ffede1f078dea4c1a33f8d3af35d82613e988
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-12 12:38:20 -0300

    Desaio 78: Maior e Menor em Listas

---
+++ Exercícios/Desafios/desafio78.py
@@ -0,0 +1,7 @@
+vls = []
+
+for i in range(0, 4):
+
+    vls.append(int(input('Digite um valor para ser armazenado na lista: ')))
+
+print(f'O maior valor foi {max(vls)}\nO menor valor foi {min(vls)}')
