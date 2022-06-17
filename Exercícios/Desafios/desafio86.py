8ff9483c3fbcdedd4a32fbb098e5d9ff6cddf339
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-16 21:32:07 -0300

    Formatação correta

--- Exercícios/Desafios/desafio86.py
+++ Exercícios/Desafios/desafio86.py
@@ -4,8 +4,14 @@ for l in range(0, 3):
     
     for c in range(0, 3):
 
-        ma[l][c] = int(input('Digite: '))
+        ma[l][c] = int(input(f'Escolha o valor da posição [{l}, {c}]: '))
+        
+print('\n')
 
 for i in range(0, 3):
+
+    for c in range(0, 3):
     
-    print(ma[i])
+        print(f'[{ma[i][c]:^6}]', end='')
+    
+    print()
