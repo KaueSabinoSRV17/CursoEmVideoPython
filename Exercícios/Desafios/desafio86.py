0f2c20ff107efce2c4e8d5426e09c66ea269b559
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-06-16 21:26:00 -0300

    Desafio 86: Matriz

---
+++ Exercícios/Desafios/desafio86.py
@@ -0,0 +1,11 @@
+ma = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for l in range(0, 3):
+    
+    for c in range(0, 3):
+
+        ma[l][c] = int(input('Digite: '))
+
+for i in range(0, 3):
+    
+    print(ma[i])
